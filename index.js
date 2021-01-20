eee73abe421e5b98d8b1769b2dca6e14c6e70790
thebinario <abimaelfrrr@gmail.com>
2021-01-20 13:46:19 -0300

    construindo CRUD sem validacoes
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const badyParse = require('body-parser')
+const userRoute = require('./routes/userRoutes')
+
+const userRoutes = require('./routes/userRoutes')
+
 const app = express()
+app.use(badyParse.urlencoded({ extended: false }))
 const port = 3000
+
+userRoute(app)
+app.get('/', (req, res) => res.send('acessando api'))
 
 app.listen(port, () => console.log('API rodando na porta ' + port))
